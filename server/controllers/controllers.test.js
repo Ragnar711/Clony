01bcaa0597955de5c184d51f9ed0733683d246c8
Mohamed Aziz Bechaib <azizbechaib711@gmail.com>
2023-01-24 14:05:19 +0100

    Initialized the DB

--- server/controllers/controllers.test.js
+++ server/controllers/controllers.test.js
@@ -2,32 +2,32 @@ const request = require("supertest");
 const { app } = require("../app");
 
 describe("Post /media", () => {
-    test("should create new media - test1", async () => {
+    test("should create new media", async () => {
         const response = await request(app).post("/postMedia").send({
-            Name: "Test Media",
-            Year: "2000",
-            Review: "3",
+            Name: "Test",
+            Year: "2005",
+            Review: "4",
             MediaType: "Movie",
-            Director: "Test Director",
-            Actor1: "Test Actor 1",
-            Actor2: "Test Actor 2",
-            Actor3: "Test Actor 3",
-            Actor4: "Test Actor 4",
+            Director: "Director",
+            Actor1: "Actor 1",
+            Actor2: "Actor 2",
+            Actor3: "Actor 3",
+            Actor4: "Actor 4",
         });
         expect(response.status).toBe(200);
         expect(response.body).toEqual({
-            Name: "Test Media",
-            Year: 2000,
-            Review: 3,
+            Name: "Test",
+            Year: 2005,
+            Review: 4,
             MediaType: "Movie",
-            Director: "Test Director",
-            Actor1: "Test Actor 1",
-            Actor2: "Test Actor 2",
-            Actor3: "Test Actor 3",
-            Actor4: "Test Actor 4",
+            Director: "Director",
+            Actor1: "Actor 1",
+            Actor2: "Actor 2",
+            Actor3: "Actor 3",
+            Actor4: "Actor 4",
         });
     });
-    test("should return error if media exists - test2", async () => {
+    test("should return error if media exists", async () => {
         const response = await request(app).post("/postMedia").send({
             Name: "Test Media",
             Year: "2000",
