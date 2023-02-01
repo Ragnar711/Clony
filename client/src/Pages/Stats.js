9cbecda06452703c7b57e0ab38685ee0838cdfcf
Mohamed Aziz Bechaib <azizbechaib711@gmail.com>
2023-02-01 15:06:22 +0100

    STYLE: added line chart to the stats page

--- client/src/Pages/Stats.js
+++ client/src/Pages/Stats.js
@@ -1,8 +1,62 @@
 import React, { useState, useEffect } from "react";
+import {
+    LineChart,
+    Line,
+    XAxis,
+    YAxis,
+    CartesianGrid,
+    Tooltip,
+    Legend,
+} from "recharts";
 import "./stats.css";
 import Header from "../Components/Header";
 import { Grid, GridItem } from "@chakra-ui/react";
 import Axios from "redaxios";
+
+const chartData = [
+    {
+        name: "Page A",
+        uv: 4000,
+        pv: 2400,
+        amt: 2400,
+    },
+    {
+        name: "Page B",
+        uv: 3000,
+        pv: 1398,
+        amt: 2210,
+    },
+    {
+        name: "Page C",
+        uv: 2000,
+        pv: 9800,
+        amt: 2290,
+    },
+    {
+        name: "Page D",
+        uv: 2780,
+        pv: 3908,
+        amt: 2000,
+    },
+    {
+        name: "Page E",
+        uv: 1890,
+        pv: 4800,
+        amt: 2181,
+    },
+    {
+        name: "Page F",
+        uv: 2390,
+        pv: 3800,
+        amt: 2500,
+    },
+    {
+        name: "Page G",
+        uv: 3490,
+        pv: 4300,
+        amt: 2100,
+    },
+];
 
 const Stats = () => {
     const [moviesCount, setMoviesCount] = useState(0);
@@ -67,6 +121,41 @@ const Stats = () => {
                     </GridItem>
                     <GridItem colSpan={4} bg="papayawhip">
                         <h3>Medias per year</h3>
+                        <LineChart
+                            width={500}
+                            height={300}
+                            data={chartData}
+                            margin={{
+                                top: 5,
+                                right: 30,
+                                left: 20,
+                                bottom: 5,
+                            }}
+                        >
+                            <CartesianGrid strokeDasharray="3 3" />
+                            <XAxis dataKey="name" />
+                            <YAxis />
+                            <Tooltip />
+                            <Legend />
+                            <Line
+                                type="monotone"
+                                dataKey="pv"
+                                stroke="red"
+                                activeDot={{ r: 4 }}
+                            />
+                            <Line
+                                type="monotone"
+                                dataKey="uv"
+                                stroke="blue"
+                                activeDot={{ r: 4 }}
+                            />
+                            <Line
+                                type="monotone"
+                                dataKey="amt"
+                                stroke="green"
+                                activeDot={{ r: 4 }}
+                            />
+                        </LineChart>
                     </GridItem>
                 </Grid>
             </div>
