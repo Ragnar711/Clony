111cab1a025fdb9ec3fc40e5c6c56300d6466c5e
Ragnar711 <azizbechaib711@gmail.com>
2023-03-22 20:04:16 +0100

    STYLE: 404 page

--- client/src/Pages/404.js
+++ client/src/Pages/404.js
@@ -8,17 +8,52 @@ const NotFound = ({ setShowHeader }) => {
         setShowHeader(false);
     }, []);
     return (
-        <div className="notFound">
-            <h1>404 - Page Not Found</h1>
-            <p>The page you are looking for does not exist.</p>
-            <button
-                className="button404"
-                onClick={() => {
-                    navigate("/Home");
-                }}
-            >
-                Go Back to Home Page
-            </button>
+        <div className="cont404">
+            <section className="wrapper">
+                <div className="container">
+                    <div id="scene" className="scene" data-hover-only="false">
+                        <div className="circle" data-depth="1.2"></div>
+                        <div className="one" data-depth="0.9">
+                            <div className="content">
+                                <span className="piece"></span>
+                                <span className="piece"></span>
+                                <span className="piece"></span>
+                            </div>
+                        </div>
+                        <div className="two" data-depth="0.60">
+                            <div className="content">
+                                <span className="piece"></span>
+                                <span className="piece"></span>
+                                <span className="piece"></span>
+                            </div>
+                        </div>
+                        <div className="three" data-depth="0.40">
+                            <div className="content">
+                                <span className="piece"></span>
+                                <span className="piece"></span>
+                                <span className="piece"></span>
+                            </div>
+                        </div>
+                        <p className="p404" data-depth="0.50">
+                            404
+                        </p>
+                        <p className="p404" data-depth="0.10">
+                            404
+                        </p>
+                    </div>
+                    <div className="text">
+                        <article>
+                            <p>
+                                Uh oh! Looks like you got lost. <br />
+                                Go back to the homepage if you dare!
+                            </p>
+                            <button onClick={() => navigate("/Home")}>
+                                i dare!
+                            </button>
+                        </article>
+                    </div>
+                </div>
+            </section>
         </div>
     );
 };
