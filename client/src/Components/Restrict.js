9b43716804abf58a46e12ae4ab6a7bce8b38944a
Ragnar711 <azizbechaib711@gmail.com>
2023-04-16 16:25:41 +0100

    Restrict display resolution

---
+++ client/src/Components/Restrict.js
@@ -0,0 +1,15 @@
+import React from "react";
+import "./ResolutionRestriction.css";
+
+const ResolutionRestriction = () => {
+    return (
+        <div className="restricted-resolution">
+            <p>
+                This website is designed to work only on screens with a
+                resolution of 1920x1080 pixels.
+            </p>
+        </div>
+    );
+};
+
+export default ResolutionRestriction;
